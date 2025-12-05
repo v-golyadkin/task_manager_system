609c4746091df4ab494e8bf13702e104d7e972cd
Redream <mr.golyadkina@mail.ru>
2025-12-05 12:28:05 +0300

    Init commit

---
+++ src/main/java/com/example/task_manager_system/TaskManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_manager_system/TaskManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
